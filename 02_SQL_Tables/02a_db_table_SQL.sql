a61a440e2d11b2cff547992acaf30ddc303620d8
Arian <a.hormozfar@yahoo.de>
2024-07-30 11:30:26 +0200

    Upload .Sql File

---
+++ 02_SQL_Tables/02a_db_table_SQL.sql
@@ -0,0 +1,14 @@
+/* ------  Strukturen ----- */
+
+/* Kommentar 1 */
+#  Kommentar 2
+-- Kommentar 3
+
+/* Datenbanken auf Server anzeigen */
+SHOW DATABASES;
+
+/* DB boo löschen, falls vorhanden*/
+DROP DATABASE IF EXISTS boo;
+
+/* DB boo anlegen, falls noch nicht vorhanden*/
+CREATE DATABASE IF NOT EXISTS boo;
