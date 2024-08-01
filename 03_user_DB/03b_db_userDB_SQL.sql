0e6f5411484e6981a5106dd6eaba83d0de628c0f
Arian <a.hormozfar@yahoo.de>
2024-08-01 10:49:44 +0200

    03b Update

--- 03_user_DB/03b_db_userDB_SQL.sql
+++ 03_user_DB/03b_db_userDB_SQL.sql
@@ -15,6 +15,15 @@ CREATE TABLE IF NOT EXISTS boo.users
 /* Struktur */
 DESCRIBE boo.users;
 
+INSERT INTO boo.users(userName, userPwd, familyName, firstName)
+VALUES("max", "12", "Mustamann", "Max");
+INSERT INTO boo.users (userName, userPwd, familyName, firstName) 
+VALUES ('john', "123", 'Doe', 'John');
+INSERT INTO boo.users (userName, userPwd, familyName, firstName) 
+VALUES ('emily', "1234", 'Brown', 'Emily');
+INSERT INTO boo.users (userName, userPwd, familyName, firstName) 
+VALUES ('michael', "1235", 'Smith', 'Michael');
+
 /* Daten */
 -- Nachteil: PWDs liegen offen! 
 
