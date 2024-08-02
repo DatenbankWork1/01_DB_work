75a700129de4cf94422dd10e6f3c6534b1ee3b15
Arian <a.hormozfar@yahoo.de>
2024-08-02 10:26:18 +0200

    04c Update

--- 04-SQL-DataImport/04c_db_SELECT.sql
+++ 04-SQL-DataImport/04c_db_SELECT.sql
@@ -5,8 +5,8 @@
 SELECT * FROM boo.cats;
 
 /* UPDATE --> Änderung(en) in bestehender Tab. */
-#UPDATE boo.cats SET cat_name = "Alonzo" WHERE id = 2;
-#UPDATE boo.cats SET fur_color = "grey" WHERE id = 3;
+-- UPDATE boo.cats SET cat_name = "Alonzo" WHERE id = 2;
+-- UPDATE boo.cats SET fur_color = "grey" WHERE id = 3;
 
 /* READ - SELECT : NEUE Ergebnistabelle wird erstellt */
 #SELECT * FROM boo.cats; -- gesamte Tabelle
@@ -15,15 +15,15 @@ SELECT * FROM boo.cats;
 #SELECT cat_name AS "Katzen" FROM boo.cats; 
 
 /*Filtern durch WHERE*/
--- SELECT 
---     cat_name AS "Katzen",
---     age AS "Alter"
--- FROM boo.cats
--- #WHERE id = 1 
+SELECT 
+    cat_name AS "Katzen",
+    age AS "Alter"
+FROM boo.cats
+WHERE id = 1 
 -- #WHERE fur_color = "grey"
 -- #WHERE fur_color = "alpinaweiss"
 -- WHERE age >= 30
--- ; 
+; 
 
 /* Delete - DS löschen | Vorsicht!! */
 -- DELETE FROM boo.cats WHERE id = 2; -- Filter
